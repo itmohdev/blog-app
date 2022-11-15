7e2d38700dac7458dfdb62807a4c25cbf7c8a327
itmohdev <itmoh666@gmail.com>
2022-11-15 15:48:16 +0200

    add configuration of mongoose db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MongoosdriverModule } from './databases/mongoosdriver/mongoosdriver.module';
+import { TypeormdriverModule } from './databases/typeormdriver/typeormdriver.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, MongoosdriverModule, TypeormdriverModule],
   controllers: [AppController],
   providers: [AppService],
 })
