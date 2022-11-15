219bec3e5484cd2a3b95a17d2af55c202fb3fbe2
itmohdev <itmoh666@gmail.com>
2022-11-15 16:13:54 +0200

    add configuration of typeorm db

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,27 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MongoosdriverModule } from './databases/mongoosdriver/mongoosdriver.module';
 import { TypeormdriverModule } from './databases/typeormdriver/typeormdriver.module';
+import { ConfigModule } from '@nestjs/config';
+import * as joi from 'joi';
 
 @Module({
-  imports: [UserModule, MongoosdriverModule, TypeormdriverModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: joi.object({
+        MONGODB_URI: joi.string().required(),
+        MSSQLDB_USERNAME: joi.string().required(),
+        MSSQLDB_PASSWORD: joi.string().required(),
+        MSSQLDB_NAME: joi.string().required(),
+        MSSQLDB_HOST: joi.string().required(),
+        MSSQLDB_PORT: joi.number().required(),
+        APP_PORT: joi.number().required(),
+      }),
+    }),
+    UserModule,
+    MongoosdriverModule,
+    TypeormdriverModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/databases/typeormdriver/typeormdriver.module.ts
+++ src/databases/typeormdriver/typeormdriver.module.ts
@@ -1,4 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'mssql',
+        host: configService.get("MSSQLDB_HOST"),
+        username: configService.get("MSSQLDB_USERNAME"),
+        password: configService.get("MSSQLDB_PASSWORD"),
+        database: configService.get("MSSQLDB_NAME"),
+        entities: ['src/**/**/*.entity.{ts, js}'],
+        synchronize: true,
+        logging: true,
+        port: configService.get("MSSQLDB_PORT"),
+        extra: {},
+      }),
+    }),
+  ],
+})
 export class TypeormdriverModule {}
